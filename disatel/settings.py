2444015ffa7c8d6ccd5e058ed2a00ec5b5d40994
AR2D2-Space <androide20.7@tutanota.com>
2019-12-03 16:35:46 -0600

    LISSSTO 1

--- disatel/settings.py
+++ disatel/settings.py
@@ -25,8 +25,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ORIGIN_WHITELIST = (
-    'http://localhost:8080',
-    'http://127.0.0.1:8000'
+    'https://disatel.herokuapp.com'
 )
 
 CORS_ALLOW_METHODS = (
